c597530d51f76d0695ba576469f97c37581c4667
Alex Bolokan <alexandr.bolokan@orbility.com>
2025-10-17 10:20:18 +0300

    commit

--- src/Prometheus.Devices.Core/Connections/EmbeddedConnection.cs
+++ src/Prometheus.Devices.Core/Connections/EmbeddedConnection.cs
@@ -3,11 +3,11 @@ using Prometheus.Devices.Core.Interfaces;
 namespace Prometheus.Devices.Core.Connections
 {
     /// <summary>
-    /// Null local connection for devices that don't require transport (built-in camera, etc.)
+    /// Embedded connection for devices that don't require transport (built-in camera, etc.)
     /// </summary>
     public class EmbeddedConnection : BaseConnection
     {
-        public override string ConnectionInfo => "LOCAL:null";
+        public override string ConnectionInfo => "Embedded:null";
 
         public override Task OpenAsync(CancellationToken cancellationToken = default)
         {
@@ -22,19 +22,10 @@ namespace Prometheus.Devices.Core.Connections
             return Task.CompletedTask;
         }
 
-        public override Task<int> SendAsync(byte[] data, CancellationToken cancellationToken = default)
-        {
-            throw new NotSupportedException("NullConnection does not support sending data");
-        }
+        public override Task<int> SendAsync(byte[] data, CancellationToken cancellationToken = default) => throw new NotSupportedException("EmbeddedConnection does not support sending data");
 
-        public override Task<byte[]> ReceiveAsync(int bufferSize = 4096, CancellationToken cancellationToken = default)
-        {
-            throw new NotSupportedException("NullConnection does not support receiving data");
-        }
+        public override Task<byte[]> ReceiveAsync(int bufferSize = 4096, CancellationToken cancellationToken = default) => throw new NotSupportedException("EmbeddedConnection does not support receiving data");
 
-        public override Task<bool> PingAsync(CancellationToken cancellationToken = default)
-        {
-            return Task.FromResult(true);
-        }
+        public override Task<bool> PingAsync(CancellationToken cancellationToken = default) => Task.FromResult(true);
     }
 }
